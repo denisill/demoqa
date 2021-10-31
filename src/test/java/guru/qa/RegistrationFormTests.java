4c5af3984976c33ddd02303a9e7817cebb0840fe
Denis_illarionov <denisill90@yandex.ru>
2021-10-31 17:46:16 +0300

    add RegistrationFormTests

---
+++ src/test/java/guru/qa/RegistrationFormTests.java
@@ -0,0 +1,60 @@
+package guru.qa;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class RegistrationFormTests {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.startMaximized = true;
+    }
+
+    @BeforeEach
+    void start() {
+        open("https://demoqa.com/automation-practice-form");
+    }
+
+    @Test
+    void registrationTest() {
+
+        $("#firstName").setValue("Denis");
+        $("#lastName").setValue("Illarionov");
+        $("#userEmail").setValue("123@mt.com");
+        $("[for='gender-radio-2']").click();
+        $("#userNumber").setValue("1234567890");
+
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("May");
+        $(".react-datepicker__year-select").selectOption("2013");
+        $(".react-datepicker__day--028:not(.react-datepicker__day--outside-month)").click();
+
+        $("#subjectsInput").setValue("Math").pressEnter();
+        $("[for=hobbies-checkbox-2]").click();
+        $("#hobbiesWrapper").$(byText("Music")).click();
+        $("#uploadPicture").uploadFile(new File("src/test/resources/file1.png"));
+
+        $("#currentAddress").setValue("someAddress");
+        $("#state").click();
+        $("#react-select-3-input").setValue("Rajasthan").pressEnter();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Jaiselmer")).scrollTo().click();
+        $("#submit").click();
+
+        //Проверка данных
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(text("Denis Illarionov"), text("123@mt.com"), text("Female"),
+                text("1234567890"), text("28 May,2013"), text("Maths"), text("Reading, Music"), text("file1.png"),
+                text("someAddress"), text("Rajasthan Jaiselmer"));
+    }
+}
