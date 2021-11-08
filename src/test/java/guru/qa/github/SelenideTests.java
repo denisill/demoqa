e5311ba62e4c4656135a50e81afa5b2963a89bbe
Denis_illarionov <denisill90@yandex.ru>
2021-11-08 23:32:35 +0300

    ДЗ_4. Selenide #1

---
+++ src/test/java/guru/qa/github/SelenideTests.java
@@ -0,0 +1,45 @@
+package guru.qa.github;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SelenideTests {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.startMaximized = true;
+    }
+
+    @Test
+    public void shouldExampleCodeForJunitFive() {
+
+        //Откройте страницу Selenide в Github
+        open("https://github.com/");
+        $("[data-test-selector=nav-search-input]").setValue("selenide").pressEnter();
+        $$(".repo-list li").get(0).$("a").click();
+        $("h1").shouldHave(text("selenide / selenide"));
+
+        //Перейдите в раздел Wiki проекта
+        $("#wiki-tab").click();;
+
+        //Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions и открыть
+        $("#wiki-body").$(byText("Soft assertions")).click();
+
+        //Проверить что внутри есть пример кода для JUnit5
+        $("#wiki-body").shouldHave(text("Using JUnit5 extend test class:"));
+    }
+
+    @Test
+    public void dragAndDropTest() {
+
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").dragAndDropTo($("#column-b"));
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+    }
+}
